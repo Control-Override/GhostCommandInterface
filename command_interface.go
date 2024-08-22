d594ba7b65e8da0917713f30586bd8ecfd4a8301
Control-Override <controloverride@proton.me>
2024-08-22 17:50:01 -0600

    updated command interface

--- command_interface.go
+++ command_interface.go
@@ -2,6 +2,11 @@ package command_interface
 
 import "fmt"
 
+
+type Command interface {
+	Execute(args []string)
+}
+
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
